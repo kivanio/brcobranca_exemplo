66ae12044432211f2604011d63a58253b3642b96
Kivanio Barbosa <kivanio@gmail.com>
2009-04-08 13:15:46 -0400

    incluindo exempli para uso de hash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -62,28 +62,6 @@ class HomeController < ApplicationController
     @boleto.instrucao6 = "ACRESCER R$ 4,00 REFERENTE AO BOLETO BANCÁRIO"
     @boleto.sacado_endereco = "Av. Rubéns de Mendonça, 157 - 78008-000 - Cuiabá/MT"
 
-    # isso não funciona, coloquei um instance_eval na no contrutor Brcobranca::Boleto::Base mais acho que ele precisa passar pelos métodos
-    # Podemos criar depois um Brcobranca::Boleto::Base.load que usa um hash, ou xml ou um arquivo(com parseline)
-    #    @boleto.new :cedente => "Kivanio Barbosa",
-    #      :documento_cedente => "12345678912",
-    #      :sacado => "Claudio Pozzebom",
-    #      :sacado_documento => "12345678900",
-    #      :valor => 135.00,
-    #      :aceite => "S",
-    #      :agencia => "4042",
-    #      :conta_corrente => "61900",
-    #      :convenio => "1238798",
-    #      :numero_documento => "7777700168",
-    #      :dias_vencimento => 5,
-    #      :data_documento => "2008-02-01".to_date,
-    #      :instrucao1 => "Pagável na rede bancária até a data de vencimento.",
-    #      :instrucao2 => "Juros de mora de 2.0% mensal(R$ 0,09 ao dia)",
-    #      :instrucao3 => "DESCONTO DE R$ 29,50 APÓS 05/11/2006 ATÉ 15/11/2006",
-    #      :instrucao4 => "NÃO RECEBER APÓS 15/11/2006",
-    #      :instrucao5 => "Após vencimento pagável somente nas agências do Banco do Brasil",
-    #      :instrucao6 => "ACRESCER R$ 4,00 REFERENTE AO BOLETO BANCÁRIO",
-    #      :sacado_endereco => "Av. Rubéns de Mendonça, 157 - 78008-000 - Cuiabá/MT"
-
     formato=params[:boleto][:formato].to_sym
     headers['Content-Type']=FORMATOS_SUPORTADOS[formato]
     send_data @boleto.to(formato), :filename => "boleto_#{banco}.#{formato}"
@@ -91,6 +69,32 @@ class HomeController < ApplicationController
 
   def index
     @formatos=FORMATOS_SUPORTADOS.keys.map{|v| v.to_s}.sort.map{|v| [v,v]}.sort
+  end
+
+  def boleto_hash
+    # isso não funciona, coloquei um instance_eval na no contrutor Brcobranca::Boleto::Base mais acho que ele precisa passar pelos métodos
+    # Podemos criar depois um Brcobranca::Boleto::Base.load que usa um hash, ou xml ou um arquivo(com parseline)
+    @boleto = BancoBrasil.new :cedente => "Kivanio Barbosa",
+    :documento_cedente => "12345678912",
+    :sacado => "Claudio Pozzebom",
+    :sacado_documento => "12345678900",
+    :valor => 135.00,
+    :aceite => "S",
+    :agencia => "4042",
+    :conta_corrente => "61900",
+    :convenio => "1238798",
+    :numero_documento => "7777700168",
+    :dias_vencimento => 5,
+    :data_documento => "2008-02-01".to_date,
+    :instrucao1 => "Pagável na rede bancária até a data de vencimento.",
+    :instrucao2 => "Juros de mora de 2.0% mensal(R$ 0,09 ao dia)",
+    :instrucao3 => "DESCONTO DE R$ 29,50 APÓS 05/11/2006 ATÉ 15/11/2006",
+    :instrucao4 => "NÃO RECEBER APÓS 15/11/2006",
+    :instrucao5 => "Após vencimento pagável somente nas agências do Banco do Brasil",
+    :instrucao6 => "ACRESCER R$ 4,00 REFERENTE AO BOLETO BANCÁRIO",
+    :sacado_endereco => "Av. Rubéns de Mendonça, 157 - 78008-000 - Cuiabá/MT"
+
+    send_data @boleto.to(:pdf), :filename => "boleto.pdf"
   end
 
 end
