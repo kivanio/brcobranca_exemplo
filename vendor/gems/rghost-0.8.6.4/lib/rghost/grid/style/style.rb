29467ddc795c3d5f49c350aff6772bd72e10b4ba
Kivanio Barbosa <kivanio@gmail.com>
2009-04-20 10:10:50 -0400

    incluido google code

--- vendor/gems/rghost-0.8.6.4/lib/rghost/grid/style/style.rb
+++
@@ -1,8 +0,0 @@
-
-module RGhost::Grid 
-  module Style
-    require 'grid/style/border_lines'
-    require 'grid/style/old_forms'
-    require 'grid/style/bottom_lines'
-  end
-end
--- vendor/gems/rghost-0.8.6.4/lib/rghost/ruby_ghost_version.rb
+++
@@ -1,8 +0,0 @@
-module RGhost::VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 8
-    TINY  = 6
-    DATE=1225822346
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  
-end
